509b3caffff1af683252616b2e4b5430381fa0d4
Eyas Valdez <eyasvaldez7@gmail.com>
2019-02-16 15:10:34 -0500

    testing node.js to cloud

--- quickstart.js
+++ quickstart.js
@@ -1,14 +1,25 @@
-async function quickstart(
-    projectId = 'YOUR_PROJECT_ID', // Your Google Cloud Platform project ID
-    bucketName = 'my-new-bucket' // The name for the new bucket
-  ) {
-    // Imports the Google Cloud client library
-    const {Storage} = require('@google-cloud/storage');
+//async function quickstart(
+//    projectId = 'chromacache-231919', // Your Google Cloud Platform project ID
+//    bucketName = 'chromacache_bucket' // The name for the new bucket
+//  ) {
+//    // Imports the Google Cloud client library
+//    const {Storage} = require('@google-cloud/storage');
   
     // Creates a client
-    const storage = new Storage({projectId});
+//    const storage = new Storage({projectId});
   
     // Creates the new bucket
-    await storage.createBucket(bucketName);
-    console.log(`Bucket ${bucketName} created.`);
-  }
+//    await storage.createBucket(bucketName);
+//    console.log(`Bucket ${bucketName} created.`);
+//  }
+'use strict';
+var express = require('express');
+var quickstart = express();
+quickstart.get('/', function(req, res){
+    res.status(200).send("Hello from eyas");
+
+});
+var server = quickstart.listen(process.env.PORT || '8080', function(){
+    console.log("app listening on port %s ", server.address().port);
+    console.log('Press CTRL + C to quit');
+});
