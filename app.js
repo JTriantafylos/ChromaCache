50dbfb9dbeb556d5a69d92ec48596d86b85c6f61
James Triantafylos <JTriantafylos@gmail.com>
2019-02-17 07:35:53 -0500

    Without File Method

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-'use strict';
-let {PythonShell} = require('./node_modules/python-shell/index')
 class Color
 {
     constructor(red, green, blue)
@@ -166,57 +164,9 @@ function populateImageUrlArray(searchWord)
     request(srchrequest, { json: true }, (err, res, body) => {
     if (err) { return console.log(err); }
     console.log(body.items);
+    var itemList = body.items;
     });
     */
-
-    const api_key = "AIzaSyC37-yN0mhRqARSEDJbYC3HaanMUKNNIbw";
-    const srch_eng_id = "012928527837730696752:wzqnawdyxwc";
-    var srchRequest = "https://www.googleapis.com/customsearch/v1?key=" + api_key + "&cx=" + srch_eng_id + "&q=" + searchWord + "&searchType=image";
-
-    const fs = require('fs');
-    fs.writeFile('websites.txt', "", (err) => {
-        if (err) throw err;
-    });
-    //send srchRequest to parse_json.py and get an array of strings
-    //console.log(srchRequest);
-    let options = {
-        mode: 'text',
-        pythonPath: '/usr/bin/python3.6',
-        pythonOptions: ['-u'], // get print results in real-time
-        scriptPath: '',
-        args: [srchRequest]
-    };
-
-    PythonShell.run('parse_json.py', options, function (err, results) {
-        if (err) throw err;
-        // results is an array consisting of messages collected during execution
-        //console.log(results[0]);
-
-        let temp_urls = ""
-        var temp_url =""
-        var quote = false;
-        //turn into an array
-        //console.log(results[0]);
-
-        var i;
-        for(i =0; i< results[0].length;i++){
-        if(results[0].charAt(i) == '|'){
-            temp_urls += temp_url + ",";
-            const fs = require('fs');
-            fs.appendFile('websites.txt', temp_url + "\n", (err) => {
-                if (err) throw err;
-            });
-            temp_url = ""
-        }else{
-            temp_url += results[0].charAt(i);
-
-        }
-        }
-
-
-    });
-
-
 }
 
 // Drops root after being started since the app is being run on port 80
@@ -259,32 +209,6 @@ app.post("/api/url", function(req,res)
         search.value = req.body.value;
 
         imageUrlList = populateImageUrlArray(search.value);
-        var fs = require('fs');
-        var es = require('event-stream');
-
-        var lines = [];
-
-        var s = fs.createReadStream('websites.txt')
-            .pipe(es.split())
-            .pipe(es.mapSync(function(line) {
-                //pause the readstream
-                s.pause();
-                lines.push(line);
-                s.resume();
-                console.log(line);
-            })
-            .on('error', function(err) {
-                console.log('Error:', err);
-            })
-            .on('end', function() {
-                //console.log('Finish reading.');
-                console.log(lines);
-            })
-        );
-
-        console.log(lines);
-
-        imageUrlList = lines;
 
         findAveragePalette(imageUrlList);
 
