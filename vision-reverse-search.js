9a76653e19e2d5c2946bc1573150e555ac06a32d
Eyas Valdez <eyasvaldez7@gmail.com>
2019-02-16 21:54:58 -0500

    Slight variation with web scraping methods

---
+++ vision-reverse-search.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// [START vision_quickstart]
+async function tagSearch(imagePath) {
+  // Imports the Google Cloud client library
+  const vision = require('@google-cloud/vision');
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  // Performs label detection on the image file
+  //const [result] = await client.labelDetection('./wakeupcat.jpg');
+  const [result] = await client.labelDetection(imagePath);
+  const labels = result.labelAnnotations;
+  console.log('Labels:');
+  labels.forEach(label => console.log(label.description));
+}
+// [END vision_quickstart]
+
+//tagSearch('./resources/tree.jpg').catch(console.error);
+
+
+
+
+
+async function paletteSearch(imagePath){
+
+  // Imports the Google Cloud client library
+  const vision = require('@google-cloud/vision');
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  // Performs label detection on the image file
+  //const [result] = await client.labelDetection('./wakeupcat.jpg');
+
+  const [result] = await client.imageProperties(imagePath);
+  const colors = result.imagePropertiesAnnotation.dominantColors.colors;
+  colors.forEach(color => console.log(color));
+  
+
+}
+
+//paletteSearch('./resources/solid-red-test.jpg').catch(console.error);
+
+async function googlePalatte(keyword){
+  
+
+}
