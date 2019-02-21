d3690064f0a220b98620841dcd97f43d975b8ee0
Eyas Valdez <eyasvaldez7@gmail.com>
2019-02-20 23:53:18 -0500

    test

--- parse_json.py
+++ parse_json.py
@@ -2,18 +2,17 @@ import urllib.request as urllib2
 import sys
 import re
 
+
 class Parse_Json:
     def __init__(self, rawHTML):
-
-
-        self.rawHTML=(rawHTML)
+        self.rawHTML = (rawHTML)
         main(self)
 
     def fetch_html(self):
         rawHTML = self.rawHTML
 
         #equivalent to "link"
-        target = [34,108,105,110,107,34]
+        target = [34, 108, 105, 110, 107, 34]
         response = urllib2.urlopen(rawHTML)
         parsed_html = response.read()
 
@@ -21,45 +20,46 @@ class Parse_Json:
         urls = ""
         counter = 0
         for x in parsed_html:
-            if(x == 34):
+            if (x == 34):
                 temp_comparator = []
                 temp_counter = counter
 
                 for c in range(6):
                     temp_comparator.append(parsed_html[temp_counter])
-                    temp_counter +=1
+                    temp_counter += 1
 
-                if(target == temp_comparator):
-                    url = self.fetch_link(counter+9, parsed_html)
+                if (target == temp_comparator):
+                    url = self.fetch_link(counter + 9, parsed_html)
                     temp_urls.append(url)
-            counter+=1
+            counter += 1
 
-        temp_url =""
+        temp_url = ""
         for t in temp_urls:
             for i in t:
 
-                if(i != ']' and i != '[' and i !='\"' and i != '\'' and i != ' ' and i != '\\'):
+                if (i != ']' and i != '[' and i != '\"' and i != '\''
+                        and i != ' ' and i != '\\'):
                     temp_url += i
-            
+
             urls += temp_url + "|"
             temp_url = ""
 
+        return (urls)
 
-        return(urls)
 
+def fetch_link(self, index, parsed_html):
+    counter = index
+    temp_url = []
+    url_string = ""
+    while (parsed_html[counter] != 34):
 
-    def fetch_link(self,index, parsed_html):
-        counter = index
-        temp_url = []
-        url_string = ""
-        while(parsed_html[counter]!= 34):
+        temp_url.append(parsed_html[counter])
+        counter += 1
 
-            temp_url.append(parsed_html[counter])
-            counter+= 1
+    for x in temp_url:
+        url_string += chr(x)
+    return (url_string)
 
-        for x in temp_url:
-            url_string += chr(x)
-        return(url_string)
 
 def main(self):
     print(self.fetch_html())
