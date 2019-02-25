588907ff3e99b95a355b565fbf0e43be6ada6e50
Eyas Valdez <eyasvaldez7@gmail.com>
2019-02-25 01:03:59 -0500

    modifying/optimizing ColourGrabber

--- ColourGrabber.js
+++ ColourGrabber.js
@@ -4,16 +4,32 @@
 const fetch = require('node-fetch');
 
 //module.exports = {
-function fetchColour  (keyword, api_key, srch_eng_id){
+function fetchColour (keyword, api_key, srch_eng_id){
         
         var srchRequest = "https://www.googleapis.com/customsearch/v1?key=" + api_key + "&cx=" + srch_eng_id + "&q=" + keyword + "&searchType=image";
-        var tempString;
-        fetch(srchRequest)
+        var tempJS;
+        return(fetch(srchRequest)
         .then(res => res.json())
-        .then(post => tempString=post);
-
-        return(JSON.stringify(tempString));
+        .then(function(json){
+               tempJS = (json);
+               items = tempJS.items;
+               itemsLength = items.length;
+               URLS = [];
+               var i;
+               for(i = 0; i <itemsLength; i ++){
+                       URLS.push(items[i].link);
+               }
+               console.log(URLS);
+                   
+        }).catch(function(error) {
+                console.log('Error: ', error);
+        })
+        );
+        
+      
+       
         
 }
 
-console.log(fetchColour('red', 'AIzaSyC37-yN0mhRqARSEDJbYC3HaanMUKNNIbw','012928527837730696752:wzqnawdyxwc'));
+var alo = fetchColour('red', 'AIzaSyC37-yN0mhRqARSEDJbYC3HaanMUKNNIbw','012928527837730696752:wzqnawdyxwc');
+console.log(alo);
